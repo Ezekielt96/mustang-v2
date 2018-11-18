dd1ffb708d9bbf6a2b5859b8331de7a238aa182c
Ezekielt96 <34502804+Ezekielt96@users.noreply.github.com>
2018-11-18 12:32:27 -0600

    Create mustang-v2js.js

---
+++ mustang-v2js.js
@@ -0,0 +1,148 @@
+var contactURLArray = [];
+var contactArray = [];
+var loadingContact = 0;
+var currentContactIndex = 0; 
+
+// Functions
+function viewCurrentContact() {
+    currentContact = contactArray[currentContactIndex];
+    console.log(currentContact);
+    document.getElementById("nameID").value = currentContact.preferredName;   
+    document.getElementById("emailID").value = currentContact.email;   
+    document.getElementById("cityID").value = currentContact.city;   
+    document.getElementById("stateID").value = currentContact.state;
+    document.getElementById("zipID").value = currentContact.zip;  
+
+
+    document.getElementById("statusID").innerHTML = "Status: Viewing contact " + (currentContactIndex+1) + " of " + contactArray.length;
+}
+//Function for previous button 
+function previous() {
+    if (currentContactIndex > 0) {
+        currentContactIndex--;
+    }
+    currentContact = contactArray[currentContactIndex];
+    viewCurrentContact();
+
+}
+//funtion for next button 
+function next() {
+    if (currentContactIndex < (contactArray.length-1)) {
+        currentContactIndex++;
+    }
+    currentContact = contactArray[currentContactIndex];
+    viewCurrentContact();
+    
+  
+}
+//funtion for new button 
+function add() {
+    console.log('add()');
+
+}
+//funtion for the delete button 
+function remove() {
+    console.log('remove()');
+
+
+}
+
+function zipFocusFunction() {
+    console.log('focusFunction()');
+
+  
+}
+
+function zipBlurFunction() {
+    getPlace();
+}
+
+function keyPressed() {
+    console.log('keyPressed()');
+
+    // This type of function should be useful in search as it implements keyPressed.
+}
+
+function getPlace() {
+    var zip = document.getElementById("zipID").value
+    console.log("zip:"+zip);
+
+    console.log("function getPlace(zip) { ... }");
+    var xhr = new XMLHttpRequest();
+
+    // Register the embedded handler function
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var result = xhr.responseText;
+            console.log("result:"+result);
+            var place = result.split(', ');
+            if (document.getElementById("cityID").value == "")
+                document.getElementById("cityID").value = place[0];
+            if (document.getElementById("stateID").value == "")
+                document.getElementById("stateID").value = place[1];
+        }
+    }
+    xhr.open("GET", "getCityState.php?zip=" + zip);
+    xhr.send(null);
+}
+
+function initApplication() {
+    console.log('Sarting Mustang Version 2 '); 
+    loadIndex();
+}
+//load class index file 
+function loadIndex() {
+    // Load the Mustang index file.
+    var indexRequest = new XMLHttpRequest();
+    indexRequest.open('GET', 'https://mustang-index.azurewebsites.net/index.json');
+    indexRequest.onload = function() {
+        console.log("Index JSON:" + indexRequest.responseText);
+        document.getElementById("indexID").innerHTML = indexRequest.responseText;
+        contactIndex = JSON.parse(indexRequest.responseText);
+        for (i=0; i<contactIndex.length; i++) {
+            contactURLArray.push(contactIndex[i].ContactURL);
+        }
+        console.log("ContactURLArray: " + JSON.stringify(contactURLArray));
+        loadContacts();
+    }
+    indexRequest.send();
+}
+
+function loadContacts() {
+    // Clear the current contactArray.
+    contactArray.length = 0;
+    loadingContact = 0;
+
+   
+    if (contactURLArray.length > loadingContact) {
+        loadNextContact(contactURLArray[loadingContact]);
+    }
+}
+
+function loadNextContact(URL) {
+    console.log("URL: " + URL);
+    contactRequest = new XMLHttpRequest();
+    contactRequest.open('GET', URL);
+    contactRequest.onload = function() {
+        console.log(contactRequest.responseText);
+        var contact;
+        contact = JSON.parse(contactRequest.responseText);
+        console.log("Contact: " + contact.firstName);
+        contactArray.push(contact);
+        document.getElementById("contactsID").innerHTML = JSON.stringify(contactArray);
+        document.getElementById("statusID").innerHTML = "Status: Loading " + contact.firstName + " " + contact.lastName;
+
+        loadingContact++;
+        if (contactURLArray.length > loadingContact) {
+            loadNextContact(contactURLArray[loadingContact]);
+        }
+        else {
+            document.getElementById("statusID").innerHTML = "Status: Contacts Loaded (" + contactURLArray.length + ")";
+            viewCurrentContact()
+            console.log(contactArray);
+        
+        }
+    }
+    contactRequest.send();
+}
+
